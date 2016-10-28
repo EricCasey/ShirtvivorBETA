6a09b15246cdbca69202831ec33bbbbcf9b67097
adamrysyk <rysyk.adam@gmail.com>
2016-10-28 02:39:05 +0000

    flex and remove items to cart

--- src/components/ShoppingBagPage/ShoppingBagPage.jsx
+++ src/components/ShoppingBagPage/ShoppingBagPage.jsx
@@ -29,52 +29,78 @@ class ShoppingBagPage extends Component {
 
     render() {
         return (
-            <div className="cart-items-container">
-                <h2><i className="fa fa-shopping-bag" aria-hidden="true"></i> Shopping Bag</h2>
-
-                <div id="checkout-card">
-                    <div>
-                        <h2>SUMMARY</h2>
-                        <p>Subtotal: ${ (this.props.getSubtotal() / 100.00).toFixed(2) }</p>
-                        <p>Taxes: ${ ((this.props.getSubtotal() / 100.00) * 0.13).toFixed(2) }</p>
-                        <p>Shipping: ${ (this.props.cartList.length * 3.00).toFixed(2) }</p>
-                        <p>Total: ${ ((this.props.getSubtotal() / 100) + ((this.props.getSubtotal() / 100) * 0.13) + (this.props.cartList.length * 3.00)).toFixed(2) }</p>
+            <div className="shopping-bag-container">
+                <div className="shopping-bag-header">
+                    <div className="shopping-bag-header-content">
+                        <div className="shopping-bag-icon">
+                            <i className="fa fa-shopping-bag" aria-hidden="true"></i>
+                        </div>
+                        <div className="shopping-bag-header-text">
+                            Shopping Bag
+                        </div>
                     </div>
-
-                    <div className="checkout-button">
-                      <StripeCheckout
-                          getSubtotal={this.props.getSubtotal}
-                          cartList={this.props.cartList}
-                      />
-                    </div>
-                    <img id="stripelogo"
-                    role="presentation" src="https://2sorea2b9ama3o8i8o2afd7a-wpengine.netdna-ssl.com/wp-content/uploads/2016/03/outline@3x.png" href="http://www.stripe.com"/>
                 </div>
-                <div id="line-items">
-                    {this.props.cartList.map((product, index) => {
-                        return (
-                            <div className="cart-item" key={index}>
-                                <img
-                                    src={product.image}
-                                    alt={product.name}
-                                />
-                                <p className="operator">+</p>
-                                <img
-                                    src='http://www.clker.com/cliparts/6/f/9/8/11971486291056358595DigitaLink_Blank_T-Shirt.svg.hi.png'
-                                    alt={ product.name }
-                                />
+                <div className="shopping-bag-content">
+                    <div className="shopping-bag-items-container">
+                        <div className="shopping-bag-items">
+                            {this.props.cartList.map((product, index) => {
+                                return (
+                                    <div className="cart-item" key={index}>
+                                        <div className="cart-item-images">
+                                            <img
+                                                src={product.image}
+                                                alt={product.name}
+                                            />
+                                            <p className="operator"><b>+</b></p>
+                                            <img
+                                                src='http://www.clker.com/cliparts/6/f/9/8/11971486291056358595DigitaLink_Blank_T-Shirt.svg.hi.png'
+                                                alt={ product.name }
+                                            />
+                                        </div>
+                                        <div className="cart-item-info">
+                                            <p><b>{product.name}</b></p>
+                                            <p>${product.price_cents / 100.00}</p>
+                                        </div>
+                                        <div className="remove-item-button">
+                                            REMOVE
+                                        </div>
+                                    </div>
+                                )
+                            })
+                          }
+                        </div>
+                    </div>
 
-                                <p>
-                                    <b>{product.name}</b>
-                                </p>
-                                <p>designer: {product.user_id} </p>
-                                <p>{product.description}</p>
-                                <p>${product.price_cents / 100.00}</p>
+                    <div className="checkout-card">
+
+                        <div className="checkout-header">
+                            SUMMARY
+                        </div>
+                        <div className="checkout-info-container">
+                            <div className="checkout-info-labels">
+                                <div><b>Subtotal:</b></div>
+                                <div><b>Taxes:</b></div>
+                                <div><b>Shipping:</b></div>
+                                <div><b>Total:</b></div>
                             </div>
-                        )
-                    })
-                  }
+                            <div className="checkout-info-values">
+                                <div>${ (this.props.getSubtotal() / 100.00).toFixed(2) }</div>
+                                <div>${ ((this.props.getSubtotal() / 100.00) * 0.13).toFixed(2) }</div>
+                                <div>${ (this.props.cartList.length * 3.00).toFixed(2) }</div>
+                                <div>${ ((this.props.getSubtotal() / 100) + ((this.props.getSubtotal() / 100) * 0.13) + (this.props.cartList.length * 3.00)).toFixed(2) }</div>
+                            </div>
+                        </div>
+
+
+                        <div className="checkout-button">
+                          <StripeCheckout
+                              getSubtotal={this.props.getSubtotal}
+                              cartList={this.props.cartList}
+                          />
+                        </div>
+                    </div>
                 </div>
+
             </div>
 
         )
